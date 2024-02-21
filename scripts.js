08044125b0c25f146f48ee8cd7eb10e635386354
Shubham Bisen <155902288+ShubhamBisen21@users.noreply.github.com>
2024-02-22 02:06:40 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const output = document.getElementById("display");
+
+function appendToDisplay(input){
+    output.value += input;
+}
+
+function clearDisplay(){
+    output.value = "";
+}
+
+function calculate(){
+    try{
+        output.value = eval(output.value);
+    }
+    catch(error){
+        output.value = "error";
+    }
+    
+}
